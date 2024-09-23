e0a1c59dee655bcf05f0c1c6a44bdd1ab06ba00b
Lucas Basto <136653403+luccabasto@users.noreply.github.com>
2024-09-22 21:53:19 -0300

    Finalização do programa

---
+++ Programa/Programa/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+Console.WriteLine("Por favor, insira seu nome e sobrenome:");
+string nomeCompleto = Console.ReadLine();
+
+Console.WriteLine("Insira sua data de nascimento (dd/MM/yyyy):");
+string dataNascimentoInput = Console.ReadLine();
+
+DateTime dataNascimento;
+
+if (DateTime.TryParseExact(dataNascimentoInput, "dd/MM/yyyy", null, System.Globalization.DateTimeStyles.None, out dataNascimento))
+{
+    int idade = CalcularIdade(dataNascimento);
+
+    Console.WriteLine($"Olá, {nomeCompleto}! Sua idade é: {idade} anos.");
+}
+else
+{
+    Console.WriteLine("Data de nascimento inválida. Por favor, use o formato dd/MM/yyyy.");
+}
+
+Console.WriteLine("Pressione qualquer tecla para sair...");
+Console.ReadKey();
+
+int CalcularIdade(DateTime dataNascimento)
+{
+    DateTime dataAtual = DateTime.Now;
+    int idade = dataAtual.Year - dataNascimento.Year;
+
+    if (dataAtual < dataNascimento.AddYears(idade))
+    {
+        idade--;
+    }
+
+    return idade;
+}
